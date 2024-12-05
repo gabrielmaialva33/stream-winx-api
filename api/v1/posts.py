ced07ff61f6bae6dd6628732fec68a7888f58a4b
Maia <gabrielmaialva33@gmail.com>
2024-12-05 10:54:38 +0000

    chore: auto fixes

--- api/v1/posts.py
+++ api/v1/posts.py
@@ -48,10 +48,10 @@ async def image(message_id: int):
 
 @router.get("/posts/stream", tags=["Post"], response_class=StreamingResponse)
 async def stream_video(
-        message_id: int = Query(...),
-        document_id: int = Query(...),
-        size: int = Query(...),
-        range_header: str | None = Header(None, alias="range"),
+    message_id: int = Query(...),
+    document_id: int = Query(...),
+    size: int = Query(...),
+    range_header: str | None = Header(None, alias="range"),
 ):
     try:
         if not range_header:
