b46537343f6ab3206927f18fcdcab8f600a4b756
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-12-06 20:25:48 -0300

    refactor: tests to improve logging clarity
    Simplified and standardized log messages in test methods to enhance readability and consistency. Removed unnecessary comments that were in Portuguese and fixed indentation for better code formatting. These changes help in maintaining a clearer log for future troubleshooting and analysis.

--- tests/load_test.py
+++ tests/load_test.py
@@ -7,79 +7,67 @@ class APITests(SequentialTaskSet):
 
     @task
     def paginate_posts(self):
-        """Testa a paginação de posts."""
         with self.client.get(
-            "/api/v1/posts",
-            params={"per_page": 10, "offset_id": 0},
-            catch_response=True,
+                "/api/v1/posts",
+                params={"per_page": 10, "offset_id": 0},
+                catch_response=True,
         ) as response:
             if response.status_code == 200:
                 response.success()
-                logging.info("Paginação de posts sucedida.")
+                logging.info(f"Paginating posts successfully.")
             else:
-                logging.error(
-                    f"Falha na paginação de posts: {response.status_code}, {response.text}"
-                )
-                response.failure(f"Erro ao paginar posts: {response.status_code}")
+                logging.error(f"Erro on posts pagination: {response.status_code} {response.text}")
+                response.failure(f"Error while paginating posts: {response.status_code}")
 
     @task
     def get_image(self):
-        """Testa a obtenção de uma imagem específica."""
-        message_id = 7188  # Use o ID de uma mensagem existente
+        message_id = 7188
         with self.client.get(
-            f"/api/v1/posts/images/{message_id}", catch_response=True
+                f"/api/v1/posts/images/{message_id}", catch_response=True
         ) as response:
             if response.status_code == 200:
                 response.success()
-                logging.info(f"Imagem com message_id={message_id} obtida com sucesso.")
+                logging.info(f"Image with message_id={message_id} obtained successfully.")
             else:
-                logging.error(
-                    f"Falha ao obter imagem: {response.status_code}, {response.text}"
-                )
-                response.failure(f"Falha ao obter imagem: {response.status_code}")
+                logging.error(f"Failure to get image: {response.status_code}")
+                response.failure(f"Error while getting image: {response.status_code}")
 
     @task
     def stream_video(self):
-        """Testa o streaming de vídeo."""
-        message_id = 7189  # Use o ID de uma mensagem existente
-        document_id = 5044457385712682420  # Exemplo de documentId
-        size = 3988488346  # Exemplo de tamanho de documento
+        message_id = 7189
+        document_id = 5044457385712682420
+        size = 3988488346
         headers = {"Range": f"bytes={size - 100}-{size}"}
 
         with self.client.get(
-            f"/api/v1/posts/stream",
-            params={"message_id": message_id, "document_id": document_id, "size": size},
-            headers=headers,
-            catch_response=True,
+                f"/api/v1/posts/stream",
+                params={"message_id": message_id, "document_id": document_id, "size": size},
+                headers=headers,
+                catch_response=True,
         ) as response:
             if response.status_code == 206:
                 response.success()
-                logging.info("Streaming de vídeo obtido com sucesso.")
+                logging.info(f"Sucessefully streamed video")
             else:
-                logging.error(
-                    f"Falha no streaming de vídeo: {response.status_code}, {response.text}"
-                )
+                logging.error(f"Erro on video streaming: {response.status_code} {response.text}")
                 response.failure(f"Erro no streaming de vídeo: {response.status_code}")
 
     @task
     def get_post(self):
-        """Testa a obtenção de um post específico."""
-        message_id = 7188  # Use o ID de uma mensagem existente
+        message_id = 7188
         with self.client.get(
-            f"/api/v1/posts/{message_id}",
-            headers={"Accept": "application/json"},
-            catch_response=True,
+                f"/api/v1/posts/{message_id}",
+                headers={"Accept": "application/json"},
+                catch_response=True,
         ) as response:
             if response.status_code == 200:
                 response.success()
-                logging.info(f"Post com message_id={message_id} obtido com sucesso.")
+                logging.info(f"Post with message_id={message_id} obtained successfully.")
             else:
-                logging.error(
-                    f"Falha ao obter post: {response.status_code}, {response.text}"
-                )
-                response.failure(f"Erro ao obter post: {response.status_code}")
+                logging.error(f"An error occurred while getting post: {response.status_code}")
+                response.failure(f"Error while getting post: {response.status_code}")
 
 
 class WebsiteUser(HttpUser):
     tasks = [APITests]
-    wait_time = between(1, 5)  # Intervalo de espera entre execuções das tarefas
+    wait_time = between(1, 5)
