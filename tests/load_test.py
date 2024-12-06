cd6ad8fbef02114a0068be3da79c72e7763c1c1c
Maia <gabrielmaialva33@gmail.com>
2024-12-06 23:21:29 +0000

    chore: auto fixes

--- tests/load_test.py
+++ tests/load_test.py
@@ -8,24 +8,34 @@ class APITests(SequentialTaskSet):
     @task
     def paginate_posts(self):
         """Testa a paginação de posts."""
-        with self.client.get("/api/v1/posts", params={"per_page": 10, "offset_id": 0}, catch_response=True) as response:
+        with self.client.get(
+            "/api/v1/posts",
+            params={"per_page": 10, "offset_id": 0},
+            catch_response=True,
+        ) as response:
             if response.status_code == 200:
                 response.success()
                 logging.info("Paginação de posts sucedida.")
             else:
-                logging.error(f"Falha na paginação de posts: {response.status_code}, {response.text}")
+                logging.error(
+                    f"Falha na paginação de posts: {response.status_code}, {response.text}"
+                )
                 response.failure(f"Erro ao paginar posts: {response.status_code}")
 
     @task
     def get_image(self):
         """Testa a obtenção de uma imagem específica."""
         message_id = 7188  # Use o ID de uma mensagem existente
-        with self.client.get(f"/api/v1/posts/images/{message_id}", catch_response=True) as response:
+        with self.client.get(
+            f"/api/v1/posts/images/{message_id}", catch_response=True
+        ) as response:
             if response.status_code == 200:
                 response.success()
                 logging.info(f"Imagem com message_id={message_id} obtida com sucesso.")
             else:
-                logging.error(f"Falha ao obter imagem: {response.status_code}, {response.text}")
+                logging.error(
+                    f"Falha ao obter imagem: {response.status_code}, {response.text}"
+                )
                 response.failure(f"Falha ao obter imagem: {response.status_code}")
 
     @task
@@ -37,28 +47,36 @@ class APITests(SequentialTaskSet):
         headers = {"Range": f"bytes={size - 100}-{size}"}
 
         with self.client.get(
-                f"/api/v1/posts/stream",
-                params={"message_id": message_id, "document_id": document_id, "size": size},
-                headers=headers,
-                catch_response=True) as response:
+            f"/api/v1/posts/stream",
+            params={"message_id": message_id, "document_id": document_id, "size": size},
+            headers=headers,
+            catch_response=True,
+        ) as response:
             if response.status_code == 206:
                 response.success()
                 logging.info("Streaming de vídeo obtido com sucesso.")
             else:
-                logging.error(f"Falha no streaming de vídeo: {response.status_code}, {response.text}")
+                logging.error(
+                    f"Falha no streaming de vídeo: {response.status_code}, {response.text}"
+                )
                 response.failure(f"Erro no streaming de vídeo: {response.status_code}")
 
     @task
     def get_post(self):
         """Testa a obtenção de um post específico."""
         message_id = 7188  # Use o ID de uma mensagem existente
-        with self.client.get(f"/api/v1/posts/{message_id}", headers={"Accept": "application/json"},
-                             catch_response=True) as response:
+        with self.client.get(
+            f"/api/v1/posts/{message_id}",
+            headers={"Accept": "application/json"},
+            catch_response=True,
+        ) as response:
             if response.status_code == 200:
                 response.success()
                 logging.info(f"Post com message_id={message_id} obtido com sucesso.")
             else:
-                logging.error(f"Falha ao obter post: {response.status_code}, {response.text}")
+                logging.error(
+                    f"Falha ao obter post: {response.status_code}, {response.text}"
+                )
                 response.failure(f"Erro ao obter post: {response.status_code}")
 
 
